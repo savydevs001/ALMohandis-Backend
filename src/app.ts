78969e166ac63648db350e524df9fc3235b1f48c
moizsheraz <user146712@gmail.com>
2024-10-12 12:50:08 +0500

    feat:working

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,9 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.get("/health", (req, res) => {
+  res.send("All System is running");
+});
 
 app.listen(PORT, () =>
   console.log(`server running in http://localhost:${PORT}`)
